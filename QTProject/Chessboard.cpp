065b98c499f6ed57614719a3419518d0152d8693
Jannis Bujak <jannis.bujak@grecon.de>
2023-03-29 21:26:54 +0200

    Getting there aber brauche neue bilder

--- QTProject/Chessboard.cpp
+++ QTProject/Chessboard.cpp
@@ -1,5 +1,23 @@
 #include "Chessboard.h"
 
+std::vector<QPixmap> Chessboard::create_pixmaps(int a_xSectors, int a_ySectors, const QPixmap& a_glob_pxmp)
+{
+	std::vector<QPixmap> pxmaps;
+	int xSectorSize = a_glob_pxmp.width() / a_xSectors
+		, ySectorSize = a_glob_pxmp.height() / a_ySectors;
+	
+	for (int y = 0; y < a_ySectors; y++)
+	{
+		for (int x = 0; x < a_xSectors; x++)
+		{
+			QRect cutout_rect(x * xSectorSize, y * ySectorSize, xSectorSize, ySectorSize);
+
+			pxmaps.push_back(a_glob_pxmp.copy(cutout_rect));
+		}
+	}
+	return pxmaps;
+}
+
 Chessboard::Chessboard(QApplication& a_application, QGraphicsScene* a_scene, QSize a_windowrect)
 	: m_application(a_application)
 	, QGraphicsView(a_scene)
@@ -9,45 +27,71 @@ Chessboard::Chessboard(QApplication& a_application, QGraphicsScene* a_scene, QSi
 	{
 		m_rect_items.push_back(std::make_unique<QGraphicsRectItem>());
 		m_labels.push_back(std::make_unique<QLabel>());
-		scene()->addItem(m_rect_items.back().get());
-		scene()->addWidget(m_labels.back().get());
+		m_images.push_back(std::make_unique<QImage>());
 	}
+	for (std::unique_ptr<QGraphicsRectItem>& r : m_rect_items)
+	{
+		scene()->addItem(r.get());
+	}
+	
+	for (std::unique_ptr<QLabel>& l : m_labels)
+	{
+		scene()->addWidget(l.get());
+	}
+	
+	m_pxmaps = create_pixmaps(4, 3, QPixmap(m_filename));
+	
+}
+
+
+void Chessboard::display_rect(int x, int y, const QRectF& a_rect)
+{
+	
+	QGraphicsRectItem* rect_item = m_rect_items[y * BOARD_WIDTH + x].get();
+
+	rect_item->setRect(a_rect);
+	if ((x + y) % 2)
+		rect_item->setBrush(QColor(238, 238, 228));
+	else
+		rect_item->setBrush(QColor(234, 182, 118));
+
+}
+
+void Chessboard::display_label(int x, int y, const QRectF& a_rect, QPixmap& a_pxmp)
+{	
+	QLabel* label_item = m_labels[y * BOARD_WIDTH + x].get();
+	
+	int wTenth = a_rect.width() / 3
+		, hTenth = a_rect.height() / 3;
+	QRect cpy(a_rect.left() + wTenth
+		, a_rect.top() + hTenth
+		, a_rect.width() - (2 * wTenth)
+		, a_rect.height() - (2 * hTenth)
+	);
+	
+	label_item->setGeometry(cpy);
+	label_item->setPixmap(a_pxmp.scaled(cpy.width(), cpy.height()));
 }
 
 void Chessboard::display()
-{		
+{	
 	const size_t width = size().width()
 		, height = size().height()
 		, xSegSize = width / BOARD_WIDTH
-		, ySegSize = height / BOARD_HEIGHT;
+		, ySegSize = height / BOARD_HEIGHT;	
 	
-	const wchar_t w = L'\u2659'
-		, b = L'\u265F';
-
-
+	QRectF rect;
 	for (int y = 0; y < BOARD_HEIGHT; y++)
 	{
 		for (int x = 0; x < BOARD_WIDTH; x++)
 		{
-			
-			QGraphicsRectItem* rect_item = m_rect_items[y * BOARD_WIDTH + x].get();
-			QLabel* label = m_labels[y * BOARD_WIDTH + x].get();
+			rect = QRectF(x * xSegSize, y * ySegSize, xSegSize, ySegSize);
 
-			QRectF r(x * xSegSize, y * ySegSize, xSegSize, ySegSize);
-			rect_item->setRect(r);
-			if((x+y) % 2)
-				rect_item->setBrush(QColor(234, 182, 118));
-			else 
-				rect_item->setBrush(QColor(238, 238, 228));
-
-			QString unicode = "Hello I’ve to go";
-			qDebug() << "Unicode String: " << unicode;
-
-
-			label->setText(QString("%1").arg(w)); 
+			display_rect(x, y, rect);
+			int counter = y * BOARD_WIDTH + x;
+			display_label(x, y, rect, m_pxmaps[counter % m_pxmaps.size()]);
 		}
-	}
-	
+	}	
 	show();
 }
 
@@ -81,8 +125,6 @@ void Chessboard::print()
 
 void Chessboard::resizeEvent(QResizeEvent* event)
 {
-	qDebug() << "AAAAAGGGGGG";
-	// fitInView(0, 0, 500, 500, Qt::KeepAspectRatio);
 	display();
 	QGraphicsView::resizeEvent(event);
 }
--- QTProject/Chessboard.h
+++ QTProject/Chessboard.h
@@ -11,6 +11,7 @@
 #include <QLabel>
 
 #include <QDebug>
+#include <QFile>
 
 
 /*Drawing the playing-field here*/
@@ -21,8 +22,29 @@ private:
 	ChessGame m_chessgame;
 	std::vector<std::unique_ptr<QGraphicsRectItem>> m_rect_items;
 	std::vector<std::unique_ptr<QLabel>> m_labels;
+	std::vector<std::unique_ptr<QImage>> m_images;
+	
+	const wchar_t w = L'\u2659'
+		, b = L'\u265F';
+
+	const QChar qw = QChar(0x59, 0x26);
+
+	const QString m_filename = QString("../../../../QTProject/chess-icon/vecteezy_chess_icon.jpg");
+	// const QString m_filename = QString(":/QTProject/chess-icon/vecteezy_chess_icon.jpg");
+	
+	
+	std::vector<QPixmap> m_pxmaps;
+
+
+public:
+	static std::vector<QPixmap> create_pixmaps(int a_xSectors, int a_ySectors, const QPixmap& a_glob_pxmp);
+
 public:
 	Chessboard(QApplication& a_application, QGraphicsScene* a_scene, QSize a_windowrect);
+	
+
+	void display_rect(int x, int y, const QRectF& a_rect);
+	void display_label(int x, int y, const QRectF& a_rect, QPixmap& a_pxmp);
 	void display();
 	void print();
 
